81b728a17dabc69b98ee68e3c1b334505ecd6dea
ITech777 <Andrew589epa@gmail.com>
2025-10-21 06:51:32 +0700

    Практическая работа 3: актуальная версия

---
+++ .git-msg-rewrite.sh
@@ -0,0 +1,4 @@
+﻿#!/usr/bin/env bash
+set -e
+# Replace exact commit message across all refs
+git filter-branch -f --msg-filter '\''sed -e "s/^chore: sync latest local changes$/Практическая работа 3/"'\'' -- --all
